c7bc53a23733c3718d3000da03ec7d34fba904eb
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-05-31 10:43:56 -0600

    Model 1 updated

--- Pokedex_Codable/Pokedex_Codable/Model/Pokemon.swift
+++ Pokedex_Codable/Pokedex_Codable/Model/Pokemon.swift
@@ -6,42 +6,37 @@
 //
 
 import Foundation
+// TOP LEVEL DICTIONARY
+// End point 1
+struct Pokedex: Decodable {
+    let results: [ResultsDictionary]
+}
 
-class Pokemon {
+struct ResultsDictionary: Decodable {
+    let name: String
+    let url: String
+}
+// EndPoint 2
+struct Pokemon: Decodable {
     enum Keys: String {
         case name = "name"
         case id = "id"
-        case moves = "moves"
         case sprites = "sprites"
-        case frontShiny = "front_shiny"
-        case move = "move"
     }
-    
     let name: String
     let id: Int
-    let moves: [String]
-    let spritePath: String
-    
-    init?(dictionary:[String:Any]) {
-        guard let name = dictionary[Keys.name.rawValue] as? String,
-              let id = dictionary[Keys.id.rawValue] as? Int,
-              let spriteDict = dictionary[Keys.sprites.rawValue] as? [String:Any],
-              let spritePosterPath = spriteDict[Keys.frontShiny.rawValue] as? String,
-              let movesArray = dictionary[Keys.moves.rawValue] as? [[String:Any]] else {return nil}
-        
-        var moves: [String] = []
-        
-        for dict in movesArray {
-            guard let moveDict = dict[Keys.move.rawValue] as? [String:Any],
-                  let moveName = moveDict[Keys.name.rawValue] as? String else {return nil}
-            
-            moves.append(moveName)
-        }
-        
-        self.name = name
-        self.id = id
-        self.moves = moves
-        self.spritePath = spritePosterPath
-        
+    let sprites: Sprites
+}
+
+struct Sprites: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+        case frontShiny = "front_shiny"
+        case frontFemale = "front_female"
+        case frontShinyFemale = "front_shiny_female"
     }
+    let frontDefault: String
+    let frontShiny: String
+    let frontFemale: String?
+    let frontShinyFemale: String?
 }
